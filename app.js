a7651b2ae23569cc34380576b493ead98319c638
Ajuna Prathap <ajunaprathap@gmail.com>
2020-02-06 12:20:53 -0500

    updated javascript and hamburber menu

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const navLinks = document.querySelectorAll('.nav-links li');
       if(link.style.animation){
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards`;
       }
     });
     //Burger animation
