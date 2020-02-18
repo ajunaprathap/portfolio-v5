45a12742be29cc5137d16274012e587a4abbbe81
Ajuna Prathap <ajunaprathap@gmail.com>
2020-02-18 14:36:31 -0500

    corrected horizontal scroll issue

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const toggleClasses = () => {
   //Burger animation
   burger.classList.toggle('toggle');
   container.classList.toggle('no-scroll');
+
+  //Toggle Nav
+  nav.classList.toggle('nav-active');
 }
 
   burger.addEventListener('click', () => {
-    //Toggle Nav
-    nav.classList.toggle('nav-active');
-
     //Animate Links
     navLinks.forEach((link, index) => {
 
@@ -22,14 +22,13 @@ const toggleClasses = () => {
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards`;
       }
-    link.addEventListener('click', () => {
-      if (nav.classList.contains('nav-active')) {
-        toggleClasses ();
-      }
+      link.addEventListener('click', () => {
+        if (nav.classList.contains('nav-active')) {
+          toggleClasses ();
+        }
+      });
     });
-  });
   toggleClasses();
-});
+ });
 }
-
 navSlide();
